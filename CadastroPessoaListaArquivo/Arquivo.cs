99c09fa5a4b503563b2f046e2160757b7937afd0
ricar <ricar@FROHLICHNB>
2023-11-22 21:40:18 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroPessoaListaArquivo/Arquivo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroPessoaListaArquivo
+{
+    internal class Arquivo
+    {
+        private string _nome;
+        private StreamWriter _sw;
+        private StreamReader _sr;
+        public string Mensagem { get; set; }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    _nome = value;
+                }
+            }
+        }
+        public Arquivo(string nome)
+        {
+            Nome = nome;
+        }
+
+        public void CriaAbreArquivo()
+        {
+            _sw = new StreamWriter("C:\\Arquivo\\" + Nome + ".txt", true, Encoding.UTF8);
+        }
+
+        public void LerArquivo()
+        {
+            string linha;
+            _sr = new StreamReader("C:\\Arquivo\\" + Nome + ".txt");
+            linha = _sr.ReadLine();
+            while (linha != null)
+            {
+                Console.WriteLine(linha);
+                linha = _sr.ReadLine();
+            }
+            _sr.Close();
+        }
+
+        public void GravarMensagem(string mensagem)
+        {
+            _sw.WriteLine(mensagem);
+        }
+
+        public void FecharArquivo()
+        {
+            _sw.Close();
+        }
+
+        public string[] LerTodasLinhas()
+        {
+            string[] array = File.ReadAllLines("C:\\Arquivo\\" + Nome + ".txt");
+            return array;
+        }
+
+    }
+}
---
+++ CadastroPessoaListaArquivo/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace CadastroPessoaListaArquivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, mensagem;
+            Console.WriteLine("Digite o nome do arquivo: ");
+            nome = Console.ReadLine();
+            Arquivo a = new Arquivo(nome);
+            int op;
+            List<Pessoa> lista = new List<Pessoa>();
+            while (true)
+            {
+                Console.WriteLine("Menu do sistema de pessoas: ");
+                Console.WriteLine("1 - Cadastrar uma pessoa");
+                Console.WriteLine("2 - para ler todas as pessoas");
+                op = int.Parse(Console.ReadLine());
+                if (op == 1)
+                {
+                    
+                    Pessoa p = new Pessoa();
+                    Console.WriteLine("Digite o id da pessoa: ");
+                    p.Id = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite o nome da pessoa: ");
+                    p.Nome = Console.ReadLine();
+                    Console.WriteLine("Digite o e-mail da pessoa: ");
+                    p.Email = Console.ReadLine();
+                    a.CriaAbreArquivo();
+                    mensagem = p.Id+";"+p.Nome+";"+p.Email;
+                    a.GravarMensagem(mensagem);
+                    a.FecharArquivo();
+                    lista.Add(p);
+                }
+                if (op == 2)
+                {
+                    lista.Clear();
+                    string[] vetorPessoa = a.LerTodasLinhas();
+                    for(int i = 0; i< vetorPessoa.Length; i++)
+                    {   
+                        Pessoa paux = new Pessoa();
+                        string[] auxiliar = vetorPessoa[i].Split(';');
+                        paux.Id = Convert.ToInt32(auxiliar[0]);
+                        paux.Nome = auxiliar[1];
+                        paux.Email = auxiliar[2];
+                        lista.Add(paux);
+                    }
+
+                    foreach (Pessoa item in lista)
+                    {
+                        Console.Write("ID: " + item.Id);
+                        Console.Write(" - Nome: " + item.Nome);
+                        Console.WriteLine(" - E-mail: " + item.Email);
+                    }
+                }
+            }
+        }
+    }
+}
